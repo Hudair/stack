0b4d3c7b40b84498e4ab29b351b9fb3da92539c4
Mohammed Hudair <47048229+Hudair@users.noreply.github.com>
2022-02-23 20:59:36 +0300

    first commit

---
+++ app/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '12345678');
+define('DB_NAME', 'stack');
+define('TIMEZONE', 'Asia/Aden');
+define('ENCRYPTION_KEY', '5b80ca976a729308bb507f6c0c76b9f5');
